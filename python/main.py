88ac847fd9a53e25253f72216ee2fad3fa8353a2
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-23 20:18:36 +0100

    Use byte strings for Python example.

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
 import ctypes
 
 stringtools = ctypes.CDLL("../target/libstringtools-261cf0fc14ce408c.so")
-print(stringtools.count_substrings("banana", "na"))
+print(stringtools.count_substrings(b"banana", b"na"))
