1c8730d8cea8d633f2a308f5adf755eb846d6392
Zbigniew Siciarz <antyqjon@gmail.com>
2014-12-23 19:52:56 +0100

    extern "C"

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::c_str::CString;
 use libc::c_char;
 
 #[no_mangle]
-pub extern fn count_substrings(value: *const c_char, substr: *const c_char) -> i32 {
+pub extern "C" fn count_substrings(value: *const c_char, substr: *const c_char) -> i32 {
     let c_value = unsafe { CString::new(value, false) };
     let c_substr = unsafe { CString::new(substr, false) };
     match c_value.as_str() {
