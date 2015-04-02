c246ac6dda329a6c83b30b04b9c98117931c9a89
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-04-02 13:19:14 +0200

    Modernized lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
+#![feature(collections, libc)]
+
 extern crate libc;
 
-use std::c_str::CString;
+use std::ffi::CStr;
+use std::str;
 use libc::c_char;
 
 #[no_mangle]
 pub extern "C" fn count_substrings(value: *const c_char, substr: *const c_char) -> i32 {
-    let c_value = unsafe { CString::new(value, false) };
-    let c_substr = unsafe { CString::new(substr, false) };
-    match c_value.as_str() {
-        Some(value) => match c_substr.as_str() {
-            Some(substr) => value.match_indices(substr).count() as i32,
-            None => -1,
+    let c_value = unsafe { CStr::from_ptr(value).to_bytes() };
+    let c_substr = unsafe { CStr::from_ptr(substr).to_bytes() };
+    match str::from_utf8(c_value) {
+        Ok(value) => match str::from_utf8(c_substr) {
+            Ok(substr) => value.match_indices(substr).count() as i32,
+            Err(_) => -1,
         },
-        None => -1,
+        Err(_) => -1,
     }
 }
