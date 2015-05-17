eb6026d2b8bba080d3daec9d3b73438884eaf52c
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-05-17 22:50:48 +0200

    Removed globbing for library name.

--- node/main.js
+++ node/main.js
@@ -1,12 +1,6 @@
 var ffi = require('ffi');
-var glob = require('glob');
 
-var library_name = glob.sync('../target/debug/libstringtools-*.so')[0];
-if (!library_name) {
-    console.log("Couldn\'t find dynamic library");
-    process.exit(1);
-}
-
+var library_name = '../target/debug/libstringtools.so';
 var stringtools = ffi.Library(library_name, {
       'count_substrings': ['int', ['string', 'string']]
 });
--- python/main.py
+++ python/main.py
@@ -1,12 +1,5 @@
 import ctypes
-import glob
-import sys
 
-try:
-    library_name = glob.glob("../target/debug/libstringtools-*.so")[0]
-except IndexError:
-    print("Couldn't find dynamic library")
-    sys.exit(1)
-else:
-    stringtools = ctypes.CDLL(library_name)
-    print(stringtools.count_substrings(b"banana", b"na"))
+library_name = "../target/debug/libstringtools.so"
+stringtools = ctypes.CDLL(library_name)
+print(stringtools.count_substrings(b"banana", b"na"))
