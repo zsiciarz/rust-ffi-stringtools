569b981c8d747d447b9ee8e415c98c182380ae56
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-06-01 13:30:16 +0200

    Added C# example.

---
+++ c_sharp/main.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Runtime.InteropServices;
+
+public class StringTools
+{
+    [DllImport("../target/debug/libstringtools.so")]
+    public static extern Int32 count_substrings(string value, string substr);
+
+    static public void Main()
+    {
+        Console.WriteLine(count_substrings("banana", "na"));
+    }
+}
