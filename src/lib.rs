44ad337059fa03abd57be05b003618d492fe2b9f
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-06-12 23:44:25 +0200

    Feature-less implementation of count_substrings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(collections)]
-
 extern crate libc;
 
 use std::ffi::CStr;
@@ -12,9 +10,22 @@ pub extern "C" fn count_substrings(value: *const c_char, substr: *const c_char) 
     let c_substr = unsafe { CStr::from_ptr(substr).to_bytes() };
     match str::from_utf8(c_value) {
         Ok(value) => match str::from_utf8(c_substr) {
-            Ok(substr) => value.match_indices(substr).count() as i32,
+            Ok(substr) => rust_substrings(value, substr),
             Err(_) => -1,
         },
         Err(_) => -1,
     }
+}
+
+fn rust_substrings(value: &str, substr: &str) -> i32 {
+    let mut count = 0;
+    let substr_len = substr.len();
+    let upper_bound = value.len() - substr_len + 1;
+    for c in 0..upper_bound {
+        let possible_match = &value[c..c+substr_len];
+        if possible_match == substr {
+            count += 1;
+        }
+    }
+    count
 }
